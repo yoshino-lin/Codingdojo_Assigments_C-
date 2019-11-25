de2883b99a55c6dd371206d140fdcc7de30bd116
yudong <yoshino1347716570@gmail.com>
2019-11-26 06:17:15 +0800

    done

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[10,10];
+            for(int i=0;i<10;i++){
+                for(int a=0;a<10;a++){
+                    array2D[i,a] = (i+1)*(a+1);
+                }
+            }
+            for(int i=0;i<10;i++){
+                string list_out = "[";
+                for(int a=0;a<10;a++){
+                    list_out += array2D[i,a] + ", ";
+                }
+                list_out += "]";
+                Console.WriteLine(list_out);
+            }
+        }
+    }
+}
