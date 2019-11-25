66380cea514c8a0800d2ed42841ba1586470fa30
yudong <yoshino1347716570@gmail.com>
2019-11-26 07:05:42 +0800

    upload

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> boxes = new List<object>();
+            boxes.Add(7);
+            boxes.Add(28);
+            boxes.Add(-1);
+            boxes.Add(true);
+            boxes.Add("chair");
+            int sum=0;
+            for(var i=0;i<boxes.Count;i++){
+                if(boxes[i] is int){
+                    Console.WriteLine("Int: "+boxes[i]);
+                    sum += (int)boxes[i];
+                }else if(boxes[i] is string){
+                    Console.WriteLine("String:"+boxes[i]);
+                }else if(boxes[i] is bool){
+                    Console.WriteLine("Boolean:"+boxes[i]);
+                }
+            }
+            Console.WriteLine("Sum: "+sum);
+        }
+    }
+}
