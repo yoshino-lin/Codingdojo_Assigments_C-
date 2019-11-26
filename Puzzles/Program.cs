d7b57e9ec41f10096c161db821b07f0322783b3e
yudong <yoshino1347716570@gmail.com>
2019-11-27 03:48:57 +0800

    done

--- Puzzles/Program.cs
+++ Puzzles/Program.cs
@@ -7,11 +7,12 @@ namespace Puzzles
     {
         static void Main(string[] args)
         {
-            RandomArray();
+            //Console.WriteLine(TossMultipleCoins(10));
+            //Console.WriteLine(Names()[1]);
         }
+        public static Random rand = new Random();
         public static void RandomArray(){
             int[] numArray = new int[10];
-            Random rand = new Random();
             for(int val = 0; val < 10; val++){
                 numArray[val] = rand.Next(5,26);
             }
@@ -28,6 +29,43 @@ namespace Puzzles
                 }
             }
             Console.WriteLine($"Min: {min}, Max: {max}, Avg:{(double)sum/numArray.Length}");
+        }
+        public static string TossCoin(){
+            int result = rand.Next(0,2);
+            if(result == 0){
+                Console.WriteLine("Heads");
+                return "Heads";
+            }else{
+                Console.WriteLine("Tails");
+                return "Tails";
+            }
+        }
+        public static double TossMultipleCoins(int num){
+            int head_times=0,tail_times = 0;
+            for(int i=0; i<num;i++){
+                string result_of = TossCoin();
+                if(result_of == "Heads"){
+                    head_times++;
+                }else{
+                    tail_times++;
+                }
+            }
+            return (float)head_times/num;
+        }
+        public static List<object> Names(){
+            List<object> name = new List<object>();
+            name.Add("Todd");
+            name.Add("Tiffany");
+            name.Add("Charlie");
+            name.Add("Geneva");
+            name.Add("Sydney");
+            for(int i=0;i<name.Count;i++){
+                int new_idx = rand.Next(0,name.Count);
+                string temp = (string)name[i];
+                name[i] = name[new_idx];
+                name[new_idx] = temp;
+            }
+            return name;
         }
         
     }
