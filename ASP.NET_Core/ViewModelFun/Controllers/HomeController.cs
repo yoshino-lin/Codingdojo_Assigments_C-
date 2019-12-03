43b9c0d4fd0be4cba82c79425bf44eef8dee0209
yudong <yoshino1347716570@gmail.com>
2019-12-04 06:08:40 +0800

    upload

--- ASP.NET_Core/ViewModelFun/Controllers/HomeController.cs
+++ ASP.NET_Core/ViewModelFun/Controllers/HomeController.cs
@@ -4,26 +4,72 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ViewModelFun.Models;
+using Person.Models;
 
 namespace ViewModelFun.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        [HttpGet]
+        [Route("")]
+        public ViewResult Index(){
+            string message_display ="Let's get some practice using ViewModels. This project should have 4 routes that each display a different model from our controller. We'll start out with a few simple built-in types (strings and int), and then use a custom class (User) to display a single User and a collection of Users.";
+            return View("Index",message_display);
+        }
+        
+        [HttpGet]
+        [Route("numbers")]
+        public IActionResult Numbers(){
+            // to a View that has defined a model as @model string[]
+            int[] numbers = new int[]
+            {
+            1,2,3,10,43,5
+            };
+            return View("Numbers",numbers);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        [HttpGet]
+        [Route("users")]
+        public IActionResult Names(){
+            User somePerson = new User(){
+                FirstName = "Moose",
+                LastName = "Phillips",
+            };
+            User somePerson2 = new User(){
+                FirstName = "Sarah",
+                LastName = "",
+            };
+            User somePerson3 = new User(){
+                FirstName = "Jerry",
+                LastName = "",
+            };
+            User somePerson4 = new User(){
+                FirstName = "Rene",
+                LastName = "Ricky",
+            };
+            User somePerson5 = new User(){
+                FirstName = "Barbarah",
+                LastName = "",
+            };
+            
+            List<User> names = new List<User>
+            {
+                somePerson,
+                somePerson2,
+                somePerson3,
+                somePerson4,
+                somePerson5
+            };
+            return View("Users",names);
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpGet]
+        [Route("user")]
+        public IActionResult Name(){
+            User somePerson = new User(){
+                FirstName = "Moose",
+                LastName = "Phillips",
+            };
+            return View("User",somePerson);
         }
     }
 }
---
+++ ASP.NET_Core/ViewModelFun/Models/Person.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Person.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
