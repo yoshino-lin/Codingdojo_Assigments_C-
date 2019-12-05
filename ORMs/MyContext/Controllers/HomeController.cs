300f018bcb345476e28be04c61d6d446acea9027
yudong <yoshino1347716570@gmail.com>
2019-12-06 06:58:48 +0800

    upload

---
+++ ORMs/MyContext/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyContext.Models;
+using Microsoft.EntityFrameworkCore;
+using YourNamespace.Models;
+
+namespace MyContext.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class HomeController : Controller
+    {
+        private MyContext dbContext;
+    
+        // here we can "inject" our context service into the constructor
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+    
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            List<User> AllUsers = dbContext.Users.ToList();
+            
+            return View();
+        }
+    }
+    }
+}
---
+++ ORMs/MyContext/Models/MyContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace YourNamespace.Models
+{
+    public class MyContext : DbContext
+    {
+        // base() calls the parent class' constructor passing the "options" parameter along
+        public MyContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users {get;set;}
+    }
+}
