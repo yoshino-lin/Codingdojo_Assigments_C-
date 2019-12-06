f278e4fc211bc7adfc2fa4c7c53c1c7300d1beb1
yudong <yoshino1347716570@gmail.com>
2019-12-07 05:45:10 +0800

    all broken?

--- ORMs/LoginAndRegistration/Controllers/HomeController.cs
+++ ORMs/LoginAndRegistration/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LoginAndRegistration.Models;
+using Context.Models;
 
 namespace LoginAndRegistration.Controllers
 {
