fbec463a7fb6466aa3411e6835a1b008a963f88d
yudong <yoshino1347716570@gmail.com>
2019-12-13 02:24:47 +0800

    upload

--- ORMs/TheWall/Controllers/HomeController.cs
+++ ORMs/TheWall/Controllers/HomeController.cs
@@ -10,20 +10,6 @@ namespace TheWall.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
--- ORMs/TheWall/Models/ViewModel.cs
+++ ORMs/TheWall/Models/ViewModel.cs
@@ -1,4 +1,7 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace TheWall.Models{
     public class User{
